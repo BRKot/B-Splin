49e7928f3c81d5cd3cb3fe4bd45ab3c04ace7a21
BRKot <65824266+BRKot@users.noreply.github.com>
2023-01-02 15:05:19 +0500

    Добавьте файлы проекта.

---
+++ Сплайн/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Сплайн
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 49);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество точек";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сгенерировать точки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel.Location = new System.Drawing.Point(20, 81);
+            this.panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1612, 735);
+            this.panel.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1096, 50);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(921, 50);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сгенерировать точки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1648, 831);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Сплайн/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Сплайн
+{
+    public partial class Form1 : Form
+    {
+
+        float a3, a2, a1, a0;
+        float b3, b2, b1, b0;
+
+        float step;
+
+
+        Graphics g;
+
+        Random random = new Random();
+
+        int countDote = 0;
+
+        float[,] firstLine;
+        float[,] splin;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel.CreateGraphics();
+        }
+
+        void takeCoutDote()
+        {
+            try
+            {
+                countDote = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Введены не верные данные!");
+            }
+        }
+
+        void fillLine(ref float[,] line, int k)
+        {
+
+            for (int i = 0; i < line.GetLength(0); i++)
+            {
+                line[i, 0] = random.Next(10, 900);
+                line[i, 1] = random.Next(20, 600);
+            }
+        }
+
+        void drawDote(float[,] line, Color color)
+        {
+
+            Brush brush = new SolidBrush(color);
+
+            for (int i = 0; i < line.GetLength(0); i++)
+            {
+                g.FillEllipse(brush, line[i, 0] - 10, line[i, 1] - 10, 20, 20);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            takeCoutDote();
+
+            firstLine = new float[countDote, 2];
+
+            fillLine(ref firstLine, 0);
+
+            drawDote(firstLine, Color.Black);
+
+            drawLines(firstLine, Color.Black);
+        }
+
+        void drawLines(float[,] line, Color color)
+        {
+            Pen pen = new Pen(color, 2);
+
+            for (int i = 0; i < line.GetLength(0) - 1; i++)
+            {
+                g.DrawLine(pen, line[i, 0], line[i, 1], line[i + 1, 0], line[i + 1, 1]);
+            }
+
+            g.DrawLine(pen, line[0, 0], line[0, 1], line[line.GetLength(0) - 1, 0], line[line.GetLength(0) - 1, 1]);
+        }
+
+        void drawSplin(float[,] line, Color color)
+        {
+            Pen pen = new Pen(color, 2);
+
+            for (int i = 0; i < line.GetLength(0) - 1; i++)
+            {
+                g.DrawLine(pen, line[i, 0], line[i, 1], line[i + 1, 0], line[i + 1, 1]);
+            }
+        }
+
+        void clearLines(float[,] line)
+        {
+
+            drawLines(line, panel.BackColor);
+            drawDote(line, panel.BackColor);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(panel.BackColor);
+        }
+
+        void clearSplin()
+        {
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < firstLine.GetLength(0); i++)
+            {
+                points.Add(new Point((int)firstLine[i, 0], (int)firstLine[i, 1]));
+            }
+
+            points.Add(new Point((int)firstLine[0, 0], (int)firstLine[0, 1]));
+
+            Pen pen = new Pen(panel.BackColor, 10);
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                g.DrawCurve(pen, points.ToArray());
+            }
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+
+        //    List<Point> points = new List<Point>();
+        //    for(int i = 0; i < firstLine.GetLength(0); i++)
+        //    {
+        //        points.Add(new Point((int)firstLine[i, 0], (int)firstLine[i, 1]));
+        //    }
+
+        //    //points.Add(new Point((int)firstLine[0, 0], (int)firstLine[0, 1]));
+
+        //    Pen pen = new Pen(Color.Green, 5);
+
+        //    for (int i = 0; i < points.Count; i++)
+        //    {
+        //        g.DrawClosedCurve(pen, points.ToArray());
+        //    }
+        //}
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            takeStep();
+
+            float t = 0;
+            float constT = step;
+
+            
+
+            splin = new float[countDote*100, 2];
+            PointF[] points = new PointF[100];
+
+
+            for (int j = 1; j <= countDote; j++)
+            {
+                List<PointF> p = new List<PointF>();
+
+                int x1 = j, x2, x3, x4;
+
+                t = 0;
+
+               if (j == countDote)
+               {
+                    x1 = j-1;
+                    x2 = 0;
+                    x3 = 1;
+                    x4 = 2;
+               }
+               else if (j == countDote - 1)
+               {
+                    x1 = j - 1;
+                    x2 = j;
+                    x3 = 0;
+                    x4 = 1;
+               }
+               else if (j == countDote - 2)
+               {
+                    x1 = j-1;
+                    x2 = j;
+                    x3 = j+1;
+                    x4 = 0;
+               }
+                else
+                {
+                    x1 = j - 1;
+                    x2 = j;
+                    x3 = j + 1;
+                    x4 = j + 2;
+                }
+
+                getA3(firstLine[x1, 0], firstLine[x2, 0], firstLine[x3, 0], firstLine[x4, 0]);
+                getA2(firstLine[x1, 0], firstLine[x2, 0], firstLine[x3, 0], firstLine[x4, 0]);
+                getA1(firstLine[x1, 0], firstLine[x2, 0], firstLine[x3, 0], firstLine[x4, 0]);
+                getA0(firstLine[x1, 0], firstLine[x2, 0], firstLine[x3, 0], firstLine[x4, 0]);
+
+                getB3(firstLine[x1, 1], firstLine[x2, 1], firstLine[x3, 1], firstLine[x4, 1]);
+                getB2(firstLine[x1, 1], firstLine[x2, 1], firstLine[x3, 1], firstLine[x4, 1]);
+                getB1(firstLine[x1, 1], firstLine[x2, 1], firstLine[x3, 1], firstLine[x4, 1]);
+                getB0(firstLine[x1, 1], firstLine[x2, 1], firstLine[x3, 1], firstLine[x4, 1]);
+
+                for (int i = j; i < 100; i++)
+                {
+                    PointF newP = new PointF();
+                    newP.X = ((a3 * t + a2) * t + a1) * t + a0;
+                    newP.Y = ((b3 * t + b2) * t + b1) * t + b0;
+                    p.Add(newP);
+                    t += step;
+
+                }
+
+
+
+                for (int i = 0; i < 100-2; i++)
+                {
+                    points[i].X = p[i].X;
+                    points[i].Y = p[i].Y;
+
+                }
+                g.DrawCurve(new Pen(Color.Black), points, 0.01f);
+                
+
+
+            }
+            //for (int i = 0; i < points.Length; i++)
+            //{
+            //    points[i].X = p[i].X;
+            //    points[i].Y = p[i].Y;
+
+            //}
+            //g.DrawCurve(new Pen(Color.Black), points);
+        }
+
+        void takeStep()
+        {
+            step = 0.01f;
+        }
+
+        void getA3(float x0, float x1, float x2, float x3) { a3 = (-x0 + 3 * (x1 - x2) + x3) / 6; }
+        void getA2(float x0, float x1, float x2, float x3) { a2 = (x0 - 2* x1 + x2)/2; }
+        void getA1(float x0, float x1, float x2, float x3) { a1 = (x2 - x0) / 2; }
+        void getA0(float x0, float x1, float x2, float x3) { a0 = (x0 + 4*x1 +x1*2) / 6; }
+        void getB3(float y0, float y1, float y2, float y3) { b3 = (-y0 + 3 * (y1 - y2) + y3) / 6; }
+        void getB2(float y0, float y1, float y2, float y3) { b2 = (y0 - 2 * y1 + y2) / 2; }
+        void getB1(float y0, float y1, float y2, float y3) { b1 = (y2 - y0) / 2; }
+        void getB0(float y0, float y1, float y2, float y3) { b0 = (y0 + 4 * y1 + y1 * 2) / 6; }
+
+    }
+}
